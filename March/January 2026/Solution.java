ec60ec812f1fb56c4be24783989b051907535151
BeSingularTeam <besingulardemo@gmail.com>
2026-01-14 21:28:53 +0530

    update

---
+++ March/January 2026/Solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;        
+    }
+} JumpGame {
+    
+}
