126621401493ed843cabf976580420b35c25f139
BeSingularTeam <besingulardemo@gmail.com>
2026-03-14 13:12:41 +0530

    update

---
+++ March/March/ValidateBST.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean valid(TreeNode node, long minimum, long maximum) {
+        if (node == null)
+            return true;
+
+        if (!(node.val > minimum && node.val < maximum))
+            return false;
+
+        return valid(node.left, minimum, node.val) && valid(node.right, node.val, maximum);
+    }
+}
