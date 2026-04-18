161867ae578636a9fdf051034e298ab20f8d05cb
BeSingularTeam <besingulardemo@gmail.com>
2026-04-18 14:49:12 +0530

    update

---
+++ March/April/MirroDistanceOfInteger.java
@@ -0,0 +1,17 @@
+package March.April;
+
+class Solution {
+    private int rev(int n) {
+        int a = 0;
+        while (n > 0) {
+            a = a * 10 + (n % 10);
+            n /= 10;
+        }
+        return a;
+    }
+
+    public int mirrorDistance(int n) {
+        int m = rev(n);
+        return Math.abs(m - n);
+    }
+}
